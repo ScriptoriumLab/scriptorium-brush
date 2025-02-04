e4a01dcb7dcfa407dff2049078bc411a2cb0d25f
Edward NoaLand <edwardnoaland@icloud.com>
2025-02-04 15:49:11 +0800

    feat: extract util for convert clsid to wchart and concat wcharts

---
+++ include/util.h
@@ -0,0 +1,91 @@
+#ifndef UTIL_H
+#define UTIL_H
+
+#include <Windows.h>
+#include <array>
+#include <iostream>
+
+// 将一个 4 位数字转换为对应的十六进制宽字符（大写字母）
+constexpr wchar_t nibble_to_wchar(uint8_t nibble) {
+	return nibble < 10 ? L'0' + nibble : L'A' + (nibble - 10);
+}
+
+constexpr std::array<wchar_t, 39> convert_clsid_to_wchar_t(const CLSID& guid) {
+	// 格式：{XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX}
+	// 总长度 38 个字符，加上结尾的 null 字符共 39
+	std::array<wchar_t, 39> str{};
+
+	str[0] = L'{';
+
+	// Data1: 8 个十六进制数字，位于位置 1-8
+	for (int i = 0; i < 8; ++i) {
+		int shift = (7 - i) * 4;
+		uint8_t nib = static_cast<uint8_t>((guid.Data1 >> shift) & 0xF);
+		str[1 + i] = nibble_to_wchar(nib);
+	}
+	str[9] = L'-';
+
+	// Data2: 4 个十六进制数字，位置 10-13
+	for (int i = 0; i < 4; ++i) {
+		int shift = (3 - i) * 4;
+		uint8_t nib = static_cast<uint8_t>((guid.Data2 >> shift) & 0xF);
+		str[10 + i] = nibble_to_wchar(nib);
+	}
+	str[14] = L'-';
+
+	// Data3: 4 个十六进制数字，位置 15-18
+	for (int i = 0; i < 4; ++i) {
+		int shift = (3 - i) * 4;
+		uint8_t nib = static_cast<uint8_t>((guid.Data3 >> shift) & 0xF);
+		str[15 + i] = nibble_to_wchar(nib);
+	}
+	str[19] = L'-';
+
+	// Data4: 前 2 字节形成第四组，共 4 个十六进制数字，位置 20-23
+	for (int i = 0; i < 4; ++i) {
+		uint8_t byte = guid.Data4[i];
+		str[20 + 2 * i]     = nibble_to_wchar((byte >> 4) & 0xF);
+		str[20 + 2 * i + 1] = nibble_to_wchar(byte & 0xF);
+	}
+	str[24] = L'-';
+
+	// Data4: 剩下 6 字节形成第五组，共 12 个十六进制数字，位置 23-34
+	for (int i = 2; i < 8; ++i) {
+		uint8_t byte = guid.Data4[i];
+		int pos = 25 + (i - 2) * 2;
+		str[pos]     = nibble_to_wchar((byte >> 4) & 0xF);
+		str[pos + 1] = nibble_to_wchar(byte & 0xF);
+	}
+	str[37] = L'}';
+	str[38] = L'\0';
+
+	return str;
+}
+
+template<size_t L, size_t R>
+constexpr std::array<wchar_t, L + R - 1> concat(const wchar_t (&A)[L], const wchar_t (&B)[R]) {
+	std::array<wchar_t, L + R - 1> res{};
+	for (int i = 0; i < L - 1; ++i) {
+		res[i] = A[i];
+	}
+	for (int i = 0; i < R; ++i) {
+		res[L - 1 + i] = B[i];
+	}
+
+	return res;
+}
+
+template<size_t L, size_t R>
+constexpr std::array<wchar_t, L + R - 1> concat(const wchar_t (&A)[L], const std::array<wchar_t, R>& B) {
+	std::array<wchar_t, L + R - 1> res{};
+	for (int i = 0; i < L - 1; ++i) {
+		res[i] = A[i];
+	}
+	for (int i = 0; i < R; ++i) {
+		res[L - 1 + i] = B[i];
+	}
+
+	return res;
+}
+
+#endif //UTIL_H
---
+++ tests/util/util_test.cpp
@@ -0,0 +1,19 @@
+#include "gtest/gtest.h"
+#include "util.h"
+
+constexpr CLSID CLSID_MODIAN_TEXT_SERVICE{0xf7a3b6d1, 0xec88, 0x41a2, {0x9f, 0x5d, 0x7a, 0xe, 0x3c, 0x8a, 0x7b, 0x89}};
+
+TEST(convert_clsid_to_wchart_test, should_successfully_convert_to_wchart_when_pass_a_clsid) {
+	constexpr auto res = convert_clsid_to_wchar_t(CLSID_MODIAN_TEXT_SERVICE);
+
+	ASSERT_EQ(res.size(), 39);
+	ASSERT_STREQ(res.data(), L"{F7A3B6D1-EC88-41A2-9F5D-7A0E3C8A7B89}\0");
+}
+
+TEST(concat_test, should_concat_wchart_raw_array_and_std_array_successfully) {
+	constexpr wchar_t ROOT_PATH[28]{L"SOFTWARE\\Microsoft\\CTF\\TIP\\"};
+	constexpr auto NEW_KEY_PATH = concat(ROOT_PATH, convert_clsid_to_wchar_t(CLSID_MODIAN_TEXT_SERVICE));
+
+	ASSERT_EQ(NEW_KEY_PATH.size(), 66);
+	ASSERT_STREQ(NEW_KEY_PATH.data(), L"SOFTWARE\\Microsoft\\CTF\\TIP\\{F7A3B6D1-EC88-41A2-9F5D-7A0E3C8A7B89}");
+}

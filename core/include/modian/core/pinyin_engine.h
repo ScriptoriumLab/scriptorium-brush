d52a1931b04dc30e90fbeebfcd7096cae05e6d8f
Edward NoaLand <edwardnoaland@icloud.com>
2025-02-07 16:26:44 +0800

    refactor: add override identifier

--- core/include/modian/core/pinyin_engine.h
+++ core/include/modian/core/pinyin_engine.h
@@ -8,8 +8,8 @@
 namespace modian::core {
 	class pinyin_engine : public input_engine {
 	public:
-		std::vector<std::wstring> convert(const std::wstring& input);
-		void load_dictionary(const std::string& path);
+		std::vector<std::wstring> convert(const std::wstring& input) override;
+		void load_dictionary(const std::string& path) override;
 
 	private:
 		std::unordered_map<std::wstring, std::vector<std::wstring>> dictionary_;

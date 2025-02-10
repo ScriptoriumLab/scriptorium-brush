af2d6f5f081b77f7495c76fa0d7b36bf6d6e0a98
Edward NoaLand <edwardnoaland@icloud.com>
2025-02-10 14:25:42 +0800

    feat: implement register_profile in registry

--- infra/tsf/include/modian/info/registry_info.h
+++ infra/tsf/include/modian/info/registry_info.h
@@ -15,3 +15,11 @@ inline auto concat_key_path_res = wcscat_s(CLSID_KEY, convert_clsid_to_wchar_t(C
 inline wchar_t LANGUAGE_PROFILE_KEY[LANGUAGE_PROFILE_KEY_SIZE]{L""};
 inline auto concat_lang_profile_step1 = wcscat_s(LANGUAGE_PROFILE_KEY, CLSID_KEY);
 inline auto concat_lang_profile_res = wcscat_s(LANGUAGE_PROFILE_KEY, L"\\LanguageProfile\\0x00000804\\{C00E97BF-4DD6-4C08-9D8D-BA67265F4997}");
+
+namespace modian::tsf::dll {
+	static constexpr auto MODIAN_IME_LANG_ID{MAKELANGID(LANG_CHINESE, SUBLANG_CHINESE_SIMPLIFIED)};
+
+	static constexpr WCHAR MODIAN_IME_DESC[]{L"Modian Input Method"};
+	static constexpr CLSID MODIAN_IME_CLSID{0xf7a3b6d1, 0xec88, 0x41a2, {0x9f, 0x5d, 0x7a, 0xe, 0x3c, 0x8a, 0x7b, 0x89}};
+	static constexpr GUID MODIAN_IME_GUID_PROFILE = {0xC00E97BF, 0x4DD6, 0x4C08, {0x9D, 0x8D, 0xBA, 0x67, 0x26, 0x5F, 0x49, 0x97}};
+}
--- infra/tsf/src/dll/register.cpp
+++ infra/tsf/src/dll/register.cpp
@@ -1,8 +1,10 @@
 #include "modian/tsf/dll/register.h"
 
 #include <msctf.h>
-#include <modian/info/registry_info.h>
 #include <iostream>
+#include <strsafe.h>
+
+#include "modian/info/registry_info.h"
 
 modian::tsf::dll::auto_com::auto_com() : b_init{false} {
 	HRESULT hr = CoInitialize(nullptr);
@@ -13,5 +15,54 @@ modian::tsf::dll::auto_com::auto_com() : b_init{false} {
 modian::tsf::dll::auto_com::~auto_com() {
 	if (b_init) {
 		CoUninitialize();
+	}
+}
+
+bool modian::tsf::dll::com_registration::register_profiles() {
+	HRESULT hr{S_FALSE};
+
+	ITfInputProcessorProfileMgr* input_processor_profile_mgr{nullptr};
+	hr = CoCreateInstance(CLSID_TF_InputProcessorProfiles, nullptr, CLSCTX_INPROC_SERVER, IID_ITfInputProcessorProfileMgr, reinterpret_cast<void**>(&input_processor_profile_mgr));
+	if (FAILED(hr)) {
+		return false;
+	}
+
+	WCHAR ach_icon_file[MAX_PATH] = {'\0'};
+	DWORD cch_a;
+	cch_a = GetModuleFileName(g_h_instance, ach_icon_file, MAX_PATH);
+	cch_a = cch_a >= MAX_PATH ? (MAX_PATH - 1) : cch_a;
+	ach_icon_file[cch_a] = '\0';
+
+	size_t len_of_desc{0};
+	hr = StringCchLength(MODIAN_IME_DESC, STRSAFE_MAX_CCH, &len_of_desc);
+	if (hr != S_OK) {
+		goto Exit;
+	}
+
+	// TODO: extract -12 to variable
+	hr = input_processor_profile_mgr->RegisterProfile(MODIAN_IME_CLSID, MODIAN_IME_LANG_ID, MODIAN_IME_GUID_PROFILE, MODIAN_IME_DESC, static_cast<ULONG>(len_of_desc), ach_icon_file, cch_a, static_cast<UINT>(-12), nullptr, 0, true, 0);
+
+	Exit:
+	if (input_processor_profile_mgr) {
+		input_processor_profile_mgr->Release();
+	}
+
+	return (hr == S_OK);
+}
+
+void modian::tsf::dll::com_registration::unregister_profiles() {
+	HRESULT hr{S_OK};
+
+	ITfInputProcessorProfileMgr* input_processor_profile_mgr{nullptr};
+	hr = CoCreateInstance(CLSID_TF_InputProcessorProfiles, nullptr, CLSCTX_INPROC_SERVER, IID_ITfInputProcessorProfileMgr, reinterpret_cast<void**>(&input_processor_profile_mgr));
+	if (FAILED(hr)) {
+		goto Exit;
+	}
+
+	input_processor_profile_mgr->UnregisterProfile(MODIAN_IME_CLSID, MODIAN_IME_LANG_ID, MODIAN_IME_GUID_PROFILE, 0);
+
+	Exit:
+	if (input_processor_profile_mgr) {
+		input_processor_profile_mgr->Release();
 	}
 }

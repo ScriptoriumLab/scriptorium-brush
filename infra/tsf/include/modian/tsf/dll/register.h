0ad62702dfdf52107869c5e4c83c8a461dc71411
Edward NoaLand <edwardnoaland@icloud.com>
2025-02-10 15:02:22 +0800

    refactor: resort com_registration function

--- infra/tsf/include/modian/tsf/dll/register.h
+++ infra/tsf/include/modian/tsf/dll/register.h
@@ -14,10 +14,10 @@ namespace modian::tsf::dll {
 		static bool register_profiles();
 		static void unregister_profiles();
 
-		static bool register_server();
-		static void unregister_server();
-
 		static bool register_categories();
 		static void unregister_categories();
+
+		static bool register_server();
+		static void unregister_server();
 	};
 }

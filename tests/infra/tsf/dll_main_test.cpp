bd94c8d79f5f19fcde2f8128c9d01bd6a075001a
Edward NoaLand <edwardnoaland@icloud.com>
2025-02-11 17:32:12 +0800

    tests: add test for get class object

--- tests/infra/tsf/dll_main_test.cpp
+++ tests/infra/tsf/dll_main_test.cpp
@@ -4,8 +4,8 @@
 #include <string>
 #include <vector>
 
+#include "modian/tsf/tsf_text_service.h"
 #include "modian/tsf/dll/register.h"
-
 #include "modian/info/registry_info.h"
 
 STDAPI DllRegisterServer();
@@ -96,4 +96,13 @@ TEST_F(modian_registry_test, should_successfully_unregister_modian_after_registe
 
     EXPECT_FALSE(is_reg_key_exists(HKEY_LOCAL_MACHINE, CLSID_KEY));
     EXPECT_FALSE(is_reg_key_exists(HKEY_LOCAL_MACHINE, LANGUAGE_PROFILE_KEY));
+}
+
+TEST_F(modian_registry_test, should_successfully_create_input_processor) {
+    ASSERT_EQ(DllRegisterServer(), S_OK);
+
+    modian::tsf::tsf_text_service* tsf_text_service = nullptr;
+    const auto hr = CoCreateInstance(modian::tsf::dll::MODIAN_IME_CLSID, nullptr, CLSCTX_INPROC_SERVER, IID_ITfTextInputProcessor, reinterpret_cast<void**>(&tsf_text_service));
+
+    ASSERT_EQ(hr, S_OK);
 }

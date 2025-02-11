6ad9b2b7122877bdb29fcdc0736af603fb06d7b9
Edward NoaLand <edwardnoaland@icloud.com>
2025-02-11 17:34:51 +0800

    refactor: init function logic in register.cpp

--- infra/tsf/src/dll/register.cpp
+++ infra/tsf/src/dll/register.cpp
@@ -29,7 +29,7 @@ bool modian::tsf::dll::com_registration::register_profiles() {
 	}
 
 	WCHAR ach_icon_file[MAX_PATH] = {'\0'};
-	DWORD cch_a;
+	DWORD cch_a{0};
 	cch_a = GetModuleFileName(g_h_instance, ach_icon_file, MAX_PATH);
 	cch_a = cch_a >= MAX_PATH ? (MAX_PATH - 1) : cch_a;
 	ach_icon_file[cch_a] = '\0';
@@ -41,7 +41,7 @@ bool modian::tsf::dll::com_registration::register_profiles() {
 	}
 
 	// TODO: extract -12 to variable
-	hr = input_processor_profile_mgr->RegisterProfile(MODIAN_IME_CLSID, MODIAN_IME_LANG_ID, MODIAN_IME_GUID_PROFILE, MODIAN_IME_DESC, static_cast<ULONG>(len_of_desc), ach_icon_file, cch_a, static_cast<UINT>(-12), nullptr, 0, true, 0);
+	hr = input_processor_profile_mgr->RegisterProfile(MODIAN_IME_CLSID, MODIAN_IME_LANG_ID, MODIAN_IME_GUID_PROFILE, MODIAN_IME_DESC, static_cast<ULONG>(len_of_desc), ach_icon_file, cch_a, static_cast<UINT>(-12), nullptr, 0, TRUE, 0);
 
 	Exit:
 	if (input_processor_profile_mgr) {

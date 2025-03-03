bb67824ca15cf681d1f8d207040f035f5567e831
Edward NoaLand <edwardnoaland@icloud.com>
2025-03-03 15:23:02 +0800

    style: format code

--- core/include/modian/core/engine/input_engine.h
+++ core/include/modian/core/engine/input_engine.h
@@ -6,8 +6,8 @@
 namespace modian::core {
 	class input_engine {
   	public:
-          virtual ~input_engine() = default;
+		virtual ~input_engine() = default;
 
-          virtual std::vector<std::wstring> convert(const std::wstring& input) = 0;
+		virtual std::vector<std::wstring> convert(const std::wstring& input) = 0;
 	};
 }

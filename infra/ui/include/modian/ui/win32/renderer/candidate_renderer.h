985cce8537f24a6be05d8fd50aaeb253dc8a5dcc
Edward NoaLand <edwardnoaland@icloud.com>
2025-03-06 19:15:28 +0800

    refactor: add override specifier and create default value for hwnd_

--- infra/ui/include/modian/ui/win32/renderer/candidate_renderer.h
+++ infra/ui/include/modian/ui/win32/renderer/candidate_renderer.h
@@ -7,12 +7,12 @@
 namespace modian::infra::ui::win32::renderer {
 	class candidate_renderer final : public core::renderer::candidate_renderer {
 	public:
-		void set_context(const std::shared_ptr<graphic_context>& context);
+		void set_context(const std::shared_ptr<graphic_context>& context) override;
 		void begin_frame() override;
 		void draw_list(const std::vector<std::wstring>& items) override;
 		void end_frame() override;
 	private:
-		HWND hwnd_;
+		HWND hwnd_{nullptr};
 		HWND listbox_{nullptr};
 		HDC hdc_{};
 		HFONT font_{};

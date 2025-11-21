fadd9a3547e475a57e6d7009c29856121feee5d1
Edward NoaLand <edwardnoaland@gmail.com>
2025-11-21 22:02:41 +0000

    refactor: refactor dll util code to make it clear and safe

--- infra/tsf/include/modian/tsf/dll/dll_util.h
+++ infra/tsf/include/modian/tsf/dll/dll_util.h
@@ -5,23 +5,18 @@
 #include <Windows.h>
 
 namespace modian::infra::tsf::dll::util {
-	constexpr int GUID_MAX_LEN{39};
-
+	constexpr size_t GUID_STRING_BUFFER_SIZE{39};
 	inline std::wstring convert_guid_to_string(const GUID& guid) {
-		wchar_t guid_str[GUID_MAX_LEN];
-		if (const auto res = StringFromGUID2(guid, guid_str, GUID_MAX_LEN); res == 0) {
-			throw std::runtime_error{"Cannot convert this guid"};
+		wchar_t guid_str[GUID_STRING_BUFFER_SIZE]{};
+
+		if (StringFromGUID2(guid, guid_str, ARRAYSIZE(guid_str)) == 0) {
+			throw std::runtime_error{"Failed to convert GUID to string"};
 		}
 
 		return guid_str;
 	}
 
 	inline std::wstring convert_clsid_to_string(const CLSID& clsid) {
-		LPOLESTR clsid_str = nullptr;
-		if (const auto hr = StringFromCLSID(clsid, &clsid_str); FAILED(hr)) {
-			throw std::runtime_error{"cannot convert thi clsid"};
-		}
-
-		return clsid_str;
+		return convert_guid_to_string(clsid);
 	}
 }

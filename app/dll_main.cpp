cedce9058f956e0efe3e8d0576280af6084097bd
Edward NoaLand <edwardnoaland@gmail.com>
2025-11-21 21:35:29 +0000

    refactor: remove unused comments and break

--- app/dll_main.cpp
+++ app/dll_main.cpp
@@ -8,24 +8,10 @@ BOOL WINAPI DllMain(HINSTANCE h_instance, DWORD dw_reason, [[maybe_unused]] LPVO
 	case DLL_PROCESS_ATTACH:
 		modian::infra::tsf::dll::modian_instance = h_instance;
 
-		/**
-		 * TODO: when introduce multi thread, need to add create critical logic:
-		 * if (!InitializeCriticalSectionAndSpinCount(&cs, 0)) {
-		 *     return FALSE;
-		 * }
-		 */
-
 		break;
 	case DLL_PROCESS_DETACH:
-		/**
-		 * TODO: when introduce multi thread, need to add delete critical section logic:
-		 * DeleteCriticalSection(cs);
-		 */
-		break;
 	case DLL_THREAD_ATTACH:
-		break;
 	case DLL_THREAD_DETACH:
-		break;
 	default:
 		break;
 	}

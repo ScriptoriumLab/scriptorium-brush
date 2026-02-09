61cd5eb123d86ad0e246c0d8aff47675d2669812
Edward NoaLand <edwardnoaland@gmail.com>
2026-02-09 11:48:40 +0000

    refactor: rename pipe to modian input protocol pipe

--- infra/ipc/include/modian/ipc/ipc_client.h
+++ infra/ipc/include/modian/ipc/ipc_client.h
@@ -4,7 +4,7 @@
 #include <mutex>
 
 namespace modian::brush::infra::ipc {
-	constexpr std::wstring_view DEFAULT_PIPE_NAME = L"\\\\.\\pipe\\modian_ipc_brush";
+	constexpr std::wstring_view DEFAULT_PIPE_NAME = L"\\\\.\\pipe\\modian_input_protocol_pipe";
 
 	class ipc_client {
 	public:
